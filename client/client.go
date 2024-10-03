73145aa41d1879c896dbe7d303c1528343e18a69
johannes-kuhfuss <johannes.kuhfuss@web.de>
2024-10-03 20:05:45 +0200

    WiP

--- client/client.go
+++ client/client.go
@@ -1,15 +1,68 @@
 package client
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/johannes-kuhfuss/services_utils/logger"
+)
 
 type EmberClient struct {
-	//conn net.Conn
-	IsConnected bool
-	Addr        string
+	raddr string
+	conn  net.Conn
 }
 
-func NewEmberClient(address string, port uint) *EmberClient {
+func NewEmberClient(host string, port int) (*EmberClient, error) {
 	var ec EmberClient
-	ec.Addr = fmt.Sprintf("%s:%d", address, port)
-	return &ec
+	if (port < 1) || (port > 65535) {
+		return nil, errors.New("port must be between 1 and 65535")
+	}
+	if host == "" {
+		return nil, errors.New("host must be either a host name or an IP address")
+	}
+	portStr := strconv.Itoa(port)
+	ec.raddr = net.JoinHostPort(host, portStr)
+	return &ec, nil
+}
+
+func (ec *EmberClient) IsConnected() bool {
+	return ec.conn != nil
+}
+
+func (ec *EmberClient) Connect() error {
+	if ec.IsConnected() {
+		err := errors.New("already connected")
+		logger.Error(fmt.Sprintf("Cannot connect to %v", ec.raddr), err)
+		return err
+	}
+	logger.Info(fmt.Sprintf("Trying to connect to %v...", ec.raddr))
+	conn, err := net.Dial("tcp", ec.raddr)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Could not connect to %v", ec.raddr), err)
+		return err
+	}
+	ec.conn = conn
+	logger.Info(fmt.Sprintf("Connected to %v.", ec.raddr))
+	return nil
+}
+
+func (ec *EmberClient) Disconnect() error {
+	if !ec.IsConnected() {
+		return errors.New("not connected")
+	} else {
+		logger.Info(fmt.Sprintf("Disconnecting from %v.", ec.raddr))
+		ec.conn.Close()
+		logger.Info(fmt.Sprintf("Disconnected from %v.", ec.raddr))
+		return nil
+	}
+}
+
+func (ec *EmberClient) sendBerData(data []byte) error {
+	if !ec.IsConnected() {
+		return errors.New("not connected")
+	}
+	_ = data
+	return nil
 }
--- client/client_test.go
+++ client/client_test.go
@@ -1,13 +1,88 @@
 package client
 
 import (
+	"net"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_NewEmberClient_Returns_EmberClient(t *testing.T) {
-	ec := NewEmberClient("localhost", 9000)
+func Test_NewEmberClient_WrongPort_Returns_Error(t *testing.T) {
+	ec, err := NewEmberClient("localhost", -1)
+	assert.Nil(t, ec)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "port must be between 1 and 65535", err.Error())
+}
+
+func Test_NewEmberClient_NoHost_Returns_Error(t *testing.T) {
+	ec, err := NewEmberClient("", 9000)
+	assert.Nil(t, ec)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "host must be either a host name or an IP address", err.Error())
+}
+
+func Test_NewEmberClient_HostName_Returns_EmberClient(t *testing.T) {
+	ec, err := NewEmberClient("localhost", 9000)
 	assert.NotNil(t, ec)
-	assert.EqualValues(t, "localhost:9000", ec.Addr)
+	assert.Nil(t, err)
+	assert.EqualValues(t, "localhost:9000", ec.raddr)
+}
+
+func Test_NewEmberClient_IP_Returns_EmberClient(t *testing.T) {
+	ec, err := NewEmberClient("127.0.0.1", 9000)
+	assert.NotNil(t, ec)
+	assert.Nil(t, err)
+	assert.EqualValues(t, "127.0.0.1:9000", ec.raddr)
+}
+
+func Test_Connect_CannotConnect_Returns_Error(t *testing.T) {
+	ec, _ := NewEmberClient("127.0.0.1", 9000)
+	err := ec.Connect()
+	assert.NotNil(t, err)
+	assert.EqualValues(t, false, ec.IsConnected())
+	assert.EqualValues(t, "dial tcp 127.0.0.1:9000: connectex: No connection could be made because the target machine actively refused it.", err.Error())
+}
+
+func Test_Connect_CanConnect_Returns_NoError(t *testing.T) {
+	c, _ := net.Listen("tcp", "127.0.0.1:9000")
+	ec, _ := NewEmberClient("127.0.0.1", 9000)
+	err := ec.Connect()
+	assert.Nil(t, err)
+	assert.EqualValues(t, true, ec.IsConnected())
+	ec.Disconnect()
+	c.Close()
+}
+
+func Test_Connect_AlreadyConnected_Returns_Error(t *testing.T) {
+	c, _ := net.Listen("tcp", "127.0.0.1:9000")
+	ec, _ := NewEmberClient("127.0.0.1", 9000)
+	ec.Connect()
+	err := ec.Connect()
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "already connected", err.Error())
+	ec.Disconnect()
+	c.Close()
+}
+
+func Test_Disconnect_NoConnection_Returns_Error(t *testing.T) {
+	ec, _ := NewEmberClient("127.0.0.1", 9000)
+	err := ec.Disconnect()
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "not connected", err.Error())
+}
+
+func Test_Disconnect_Connected_Returns_NoError(t *testing.T) {
+	c, _ := net.Listen("tcp", "127.0.0.1:9000")
+	ec, _ := NewEmberClient("127.0.0.1", 9000)
+	ec.Connect()
+	err := ec.Disconnect()
+	assert.Nil(t, err)
+	c.Close()
+}
+
+func Test_sendBerData_NotConnected_Returns_Error(t *testing.T) {
+	ec, _ := NewEmberClient("127.0.0.1", 9000)
+	err := ec.sendBerData(nil)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "not connected", err.Error())
 }
