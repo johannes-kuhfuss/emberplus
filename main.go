28b27ba7aeab10a1904b182015b276aaf4c50d6a
johannes-kuhfuss <johannes.kuhfuss@web.de>
2024-10-04 20:56:15 +0200

    removed original connection logic

--- main.go
+++ main.go
@@ -16,45 +16,42 @@ func main() {
 
 	ec, _ := client.NewEmberClient("127.0.0.1", 8999)
 	ec.Connect()
-	/*
-		rr, err := ember.GetRootRequest()
-		if err != nil {
-			logger.Error("error getting root request", err)
-		} else {
-			ec.Write(rr)
-		}
-		out, err := ec.Receive()
-		if err != nil {
-			logger.Error("error receiving answer", err)
-		}
-		el := ember.NewElementConnection()
-		err = el.Populate(asn1.NewDecoder(out))
-		if err != nil {
-			logger.Error("error processing answer", err)
-		}
-		data, err := el.MarshalJSON()
-		if err != nil {
-			logger.Error("error marshalling answer", err)
-		}
-		logger.Info(fmt.Sprintf("data: %v", string(data)))
-	*/
+	rr, err := ember.GetRootRequest()
+	if err != nil {
+		logger.Error("error getting root request", err)
+	} else {
+		ec.Write(rr)
+	}
+	out, err := ec.Receive()
+	if err != nil {
+		logger.Error("error receiving answer", err)
+	}
+	el := ember.NewElementConnection()
+	err = el.Populate(asn1.NewDecoder(out))
+	if err != nil {
+		logger.Error("error processing answer", err)
+	}
+	data, err := el.MarshalJSON()
+	if err != nil {
+		logger.Error("error marshalling answer", err)
+	}
+	logger.Info(fmt.Sprintf("data: %v", string(data)))
 	r2, err := ember.GetRequestByType("node", "0.2")
 	if err != nil {
 		logger.Error("error getting root request", err)
 	} else {
 		ec.Write(r2)
 	}
-	out, err := ec.Receive()
+	out, err = ec.Receive()
 	if err != nil {
 		logger.Error("error receiving answer", err)
 	}
-	//logger.Info(string(out))
 	el2 := ember.NewElementConnection()
 	err = el2.Populate(asn1.NewDecoder(out))
 	if err != nil {
 		logger.Error("error processing answer", err)
 	}
-	data, err := el2.MarshalJSON()
+	data, err = el2.MarshalJSON()
 	if err != nil {
 		logger.Error("error marshalling answer", err)
 	}
